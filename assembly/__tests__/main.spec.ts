76e555464d413d095c7aaf30a4ad42fdf7f1be5c
Smiley <80674675+smiley717@users.noreply.github.com>
2022-02-11 01:14:45 -0800

    Initialize hotfix/remove-old-test-files

---
+++ assembly/__tests__/main.spec.ts
@@ -0,0 +1,15 @@
+import { sayHi, whoSaidHi } from "../main";
+import { context, storage, VM } from "near-sdk-as";
+import { LAST_SENDER_KEY } from "../model";
+
+describe("contract", () => {
+  it("should say Hi", () => {
+    sayHi();
+    expect(VM.outcome().logs).toIncludeEqual(context.sender + " says \"Hi!\"", "logs should be updated")
+    expect(storage.get<string>(LAST_SENDER_KEY)).toBe(context.sender);
+  });
+
+  it("should return who said Hi!", () => {
+    expect(whoSaidHi()).toBe(context.sender, "last who said high should be " + context.sender);
+  });
+});
